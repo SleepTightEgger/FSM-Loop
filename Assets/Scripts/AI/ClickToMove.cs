a38632618d82811a097cb8b212e676bf779bea81
thomtare <thomtare@OA-S141-02>
2026-02-03 18:56:38 -0500

    Navmesh

---
+++ Assets/Scripts/AI/ClickToMove.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class ClickToMove : MonoBehaviour
+{
+    public NavMeshAgent navAgent;
+    public LayerMask terrainLayers;
+    private InputSystem_Actions inputActions;
+
+    private void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.Player.Attack.performed += OnAttack;
+    }
+
+    private void OnAttack(InputAction.CallbackContext context)
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        
+        if (Physics.Raycast(mouseRay, out RaycastHit hitInfo, float.MaxValue, terrainLayers))
+        {
+            navAgent.SetDestination(hitInfo.point);
+        }
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Attack.performed -= OnAttack;
+        inputActions.Disable();
+    }
+}
